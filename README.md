3381ba4078c1fdc1ff4a9dbfa2871e41202bd7ae
nathannorman-toast <nathan.norman@toasttab.com>
2023-07-19 23:27:26 -0400

    Set up proper README.md, first attempt at app.py

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
+---
+sdk: gradio
+sdk_version: 3.24.1
+app_file: app.py
+---
 
-        ---
-        sdk: gradio
-        sdk_version: THE_VERSION_OF_GRADIO
-        app_file: app.py
-        ---
-        
+# Gradio WizardVicuna Demo
+This is a simple Gradio demo that uses the Wizard Vicuna model.
--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+from transformers import pipeline
+import gradio as gr
 
-        # Paste here the code of your Gradio app
-        
+# Load the model
+chat_pipeline = pipeline("text2text-generation", model="TheBloke/Wizard-Vicuna-30B-Uncensored-GPTQ")
+
+def chat_with_model(input_text):
+    # Generate a response
+    result = chat_pipeline(input_text)
+    # Get just the text from the returned dictionary
+    response_text = result[0]['generated_text']
+    return response_text
+
+iface = gr.Interface(fn=chat_with_model, inputs="text", outputs="text")
+iface.launch()
