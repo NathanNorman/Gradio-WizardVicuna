665c8c65c8c48eebdd59d8ff80a3a6a22a077117
nathannorman-toast <nathan.norman@toasttab.com>
2023-07-24 10:26:40 -0400

    Change gradio app

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 import gradio as gr
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import torch
 
-# Make sure you have enough resources to load this model, as it's quite large
-tokenizer = AutoTokenizer.from_pretrained("ehartford/WizardLM-30B-Uncensored")
-model = AutoModelForCausalLM.from_pretrained("ehartford/WizardLM-30B-Uncensored")
+def greet(name, is_morning, temperature):
+    salutation = "Good morning" if is_morning else "Good evening"
+    greeting = f"{salutation} {name}. It is {temperature} degrees today"
+    celsius = (temperature - 32) * 5 / 9
+    return greeting, round(celsius, 2)
 
-def generate_text(prompt):
-    inputs = tokenizer.encode(prompt, return_tensors="pt")
-    outputs = model.generate(inputs, max_length=100, do_sample=True, temperature=0.7)
-    text = tokenizer.decode(outputs[0])
-    return text
-
-iface = gr.Interface(fn=generate_text,
-                     inputs=gr.inputs.Textbox(lines=2, placeholder='Enter a text prompt here...'),
-                     outputs='text')
-
-iface.launch()
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "checkbox", gr.Slider(0, 100)],
+    outputs=["text", "number"],
+)
+demo.launch()
